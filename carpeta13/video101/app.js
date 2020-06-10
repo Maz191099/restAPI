c349a79058a9eafa8b1d49b23ab39e7e2c3ab4c6
Miguel Angel Zamora Carmona <maz191099@gmail.com>
2020-06-10 18:45:30 -0500

    Commit del video 102

--- carpeta13/video101/app.js
+++ carpeta13/video101/app.js
@@ -23,3 +23,32 @@ boton1.addEventListener('click', function(){
     }
     xhr.send();
 });
+
+const boton2 = document.getElementById('boton2');
+
+boton2.addEventListener('click', function(){
+    // Creacion de objeto XMLHttpRequest
+    const xhr  = new XMLHttpRequest();
+    // Abrir conexion 
+    xhr.open('GET', 'empleados.json', true);
+
+    // carga
+    xhr.onload = function(){
+        if(this.status === 200){
+            const personal = JSON.parse(this.responseText);
+            let htmlTemplate = '';
+            personal.forEach(function(persona) {
+                htmlTemplate += `
+                <ul>
+                    <li>ID: ${persona.id}</li>
+                    <li>Nombre: ${persona.nombre}</li>
+                    <li>Empresa: ${persona.empresa}</li>
+                    <li>Actividades: ${persona.trabajo}</li>
+                </ul>
+            `;
+            })
+            document.getElementById('empleados').innerHTML = htmlTemplate;
+        }
+    }
+    xhr.send();
+});
