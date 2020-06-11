0258c6c2fb3915ce7eebf75a8d1c4b94bc416a17
Miguel Angel Zamora Carmona <maz191099@gmail.com>
2020-06-10 22:28:49 -0500

    Commit del video 112

--- carpeta16/js/app.js
+++ carpeta16/js/app.js
@@ -1,4 +1,6 @@
 document.getElementById('txtBtn').addEventListener('click', cargarTXT);
+document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
+document.getElementById('apiBTN').addEventListener('click', cargarREST);
 
 function cargarTXT(){
     fetch('datos.txt')
@@ -6,11 +8,31 @@ function cargarTXT(){
             return res.text();
         })
         .then(function(data){
-            console.log(data);
             document.getElementById('resultado').innerHTML = data;
         })
         .catch(function(error){
             console.log(error)
         });
-
 }
+
+function cargarJSON(){
+    fetch('empleados.json')
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            let html = '';
+            data.forEach(function(empleado){
+                html += `
+                <li>${empleado.nombre} ${empleado.puesto} </li>
+                `;
+            })
+            document.getElementById('resultado').innerHTML = html;
+        })
+        .catch(function(error){
+            console.log(error)
+        });
+}
+
+
+
